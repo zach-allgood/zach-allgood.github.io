c42a5585cd27c8711d54ed65e3f492fef23c840e
Zach Allgood <zach.allgood@classyllama.com>
2016-10-31 11:13:45 -0500

    Added functionality for form

--- src/js/main.js
+++ src/js/main.js
@@ -252,4 +252,24 @@ $(function(){
 
     });
 
+    /**
+     * Submit form for contacting
+     */
+
+    $('form').on('submit', function (e) {
+
+        e.preventDefault();
+
+        $.ajax({
+            type: 'post',
+            url: 'post.php',
+            data: $('form').serialize(),
+            success: function () {
+                alert('Thank you for your interest. I will respond as soon as I can.');
+            }
+        });
+
+    });
+
+
 });
