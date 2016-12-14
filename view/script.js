0ccf7b5a86876f26d6d5df46f5556076c7bdce1e
Zach <allgood@zach-allgood.local>
2016-12-14 16:46:43 -0600

    Finished layout and started on JS work

--- view/script.js
+++ view/script.js
@@ -1,9 +1,10 @@
+var lnStickyNavigation;
+
 $(document).ready(function () {
     loadLanding();
-});
-
-$(window).resize(function() {
-    loadLanding();
+    loadNavigation();
+    applyResize();
+    checkHash();
 });
 
 function loadLanding() {
@@ -12,4 +13,89 @@ function loadLanding() {
     var container = $('#landing').find('.container');
     var topHeight = ($('#landing').height() / 2) - (container.height() / 2);
     container.css({ top: topHeight});
+}
+
+function loadNavigation()
+{
+    applyClickEvent();
+    applyNavigationFixForPhone();
+    applyScrollSpy();
+    applyStickyNavigation();
+}
+
+function applyClickEvent()
+{
+    $('a[href^="#"]').on('click', function(e)
+    {
+        e.preventDefault();
+
+        if( $( $.attr(this, 'href') ).length > 0 )
+        {
+            $('html, body').animate(
+                {
+                    scrollTop: $( $.attr(this, 'href') ).offset().top
+                }, 400);
+        }
+        return false;
+    });
+}
+
+function applyNavigationFixForPhone()
+{
+    $('.navbar li a').click(function(event)
+    {
+        $('.navbar-collapse').removeClass('in').addClass('collapse');
+    });
+}
+
+function applyScrollSpy()
+{
+    $('#side-nav').on('activate.bs.scrollspy', function()
+    {
+        window.location.hash = $('.nav .active a').attr('href').replace('#', '#/');
+    });
+}
+
+function applyStickyNavigation()
+{
+    lnStickyNavigation = $('.scroll-down').offset().top + 20;
+
+    $(window).on('scroll', function()
+    {
+        stickyNavigation();
+    });
+
+    stickyNavigation();
+}
+
+function stickyNavigation()
+{
+    if($(window).scrollTop() > lnStickyNavigation)
+    {
+        $('body').addClass('fixed');
+    }
+    else
+    {
+        $('body').removeClass('fixed');
+    }
+}
+
+function applyResize()
+{
+    $(window).on('resize', function()
+    {
+        lnStickyNavigation = $('.scroll-down').offset().top + 20;
+
+        $('#landing').css({ height: ($(window).height()) +'px' });
+    });
+}
+
+function checkHash()
+{
+    lstrHash = window.location.hash.replace('#/', '#');
+
+    if($('a[href="'+ lstrHash +'"]').length > 0)
+    {
+        $('a[href="'+ lstrHash +'"]').trigger('click');
+    }
 }
